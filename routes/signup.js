69f63c3de6c6c655e1c7c0ab8cce798b12cde172
Sujith Vadakkepat <sujithvm24@gmail.com>
2017-12-03 21:47:02 -0800

    refactor

--- routes/signup.js
+++ routes/signup.js
@@ -8,42 +8,10 @@ var GameSequenceGenerator = require('../utils/game_sequence_generator')
 /* GET signup page. */
 router.get('/', function(req, res, next) {
     if (req.query.name && req.query.email && req.query.authentication_type) {
-        var token = crypto.createHash('md5').update(req.query.name).digest('hex');
         if (req.query.authentication_type == "game") {
-            UserGame.findOne({token: token}, function (err, user) {
-                if (!user) {
-                    var obj = new Object();
-                    obj.name = req.query.name;
-                    obj.email = req.query.email;
-                    obj.token = token;
-                    obj.data = GameSequenceGenerator()
-
-                    // var p1 = new Object()
-                    // p1.encoding = "SDFJKL"
-                    // p1.pattern = [0, 1, 0, 2, 3, 4, 5]
-                    // p1.results = []
-
-                    // var p2 = new Object()
-                    // p2.encoding = "LSDKFJ"
-                    // p2.pattern = [0, 5, 0, 1, 4, 2, 3]
-                    // p2.results = []
-
-                    // obj.patterns.push(p1);
-                    // obj.patterns.push(p2);
-                    
-                    UserGame(obj).save(function(err) {
-                        if (!err) {
-                            res.redirect("/game/train?token=" + token);
-                        } else {
-                            res.render('error', { title: 'Error' });
-                        }
-                    });
-                } else {
-                    res.redirect("/game/train?token=" + token);
-                }
-            });
+            res.redirect("/game/train?name=" + req.query.name + "&email=" + req.query.email);
         } else if (req.query.authentication_type == "word_completion") {
-            res.redirect("/word/train?token=" + token);
+            res.redirect("/word/train?name=" + req.query.name + "&email=" + req.query.email);
         } else if (req.query.authentication_type == "poc") {
             res.render('signup', { title: 'Signup' });
         } else {
--- routes/word.js
+++ routes/word.js
@@ -4,15 +4,13 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/train', function(req, res, next) {
-
-  request("http://localhost:5000/signup?user=" + req.query.token, function (error, response, body) {
+  request("http://localhost:5000/signup?user=" + req.query.name, function (error, response, body) {
     if (error) {
       res.render('error')
     } else {
       res.render('word', { data: body});
     }
   });
-  
 });
 
 module.exports = router;
